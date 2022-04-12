c600b5f9c5854ba533134375e79f0f4919d0a550
guilzera <guiguidefarias@hotmail.com>
2022-04-12 14:20:24 -0300

    Mini calculadora v1

---
+++ Program.cs
@@ -0,0 +1,136 @@
+﻿using System.Globalization;
+
+Menu();
+
+static void Menu()
+{
+	Console.Clear();
+	
+	Console.WriteLine("O que deseja fazer?");
+	Console.WriteLine("1 - Soma");
+	Console.WriteLine("2 - Subtração");
+	Console.WriteLine("3 - Divisão");
+	Console.WriteLine("4 - Multiplicação");
+	Console.WriteLine("5 - Exponenciação");
+	Console.WriteLine("6 - Raiz");
+	
+	Console.WriteLine("7 - Sair");
+	
+	Console.WriteLine("-----------");
+	Console.Write("Selecione uma opção: ");
+	short res = short.Parse(Console.ReadLine());
+	
+	switch(res)
+	{
+		case 1: Soma(); break;
+		case 2: Subtracao(); break;
+		case 3: Divisao(); break;
+		case 4: Multiplicacao(); break;
+		case 5: Potenciacao(); break;
+		case 6: Raiz(); break;
+		case 7: Environment.Exit(0); break;
+		default: Menu(); break;
+	}
+}
+
+static void Soma()
+{
+	Console.Clear();
+
+	Console.Write("Primeiro valor: ");
+	double v1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+	Console.Write("Segundo valor: ");
+	double v2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+	Console.WriteLine();
+	double resultado = v1 + v2;
+
+	Console.WriteLine($"O resultado da soma é: {resultado}");
+	Console.ReadKey();
+	Menu();
+}
+
+static void Subtracao()
+{
+	Console.Clear();
+	
+	Console.Write("Primeiro valor: ");
+	double v1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+	
+	Console.Write("Segundo valor: ");
+	double v2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+	
+	Console.WriteLine();
+	double resultado = v1 - v2;
+	
+	Console.WriteLine($"O resultado da subtração é: {resultado}");
+	Console.ReadKey();
+	Menu();
+}
+
+static void Divisao()
+{
+	Console.Clear();
+	
+	Console.Write("Primeiro valor: " );
+	double v1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+	
+	Console.Write("Segundo valor: " );
+	double v2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+	
+	Console.WriteLine();
+	double resultado = v1 / v2;
+	
+	Console.WriteLine($"O resultado da divisão é: {resultado}");
+	Console.ReadKey();
+	Menu();
+}
+
+static void Multiplicacao()
+{
+	Console.Clear();
+	
+	Console.Write("Primeiro valor: " );
+	double v1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+	
+	Console.Write("Segundo valor: " );
+	double v2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+	
+	Console.WriteLine();
+	double resultado = v1 * v2;
+	
+	Console.WriteLine($"O resultado da multiplicação é: {resultado}");
+	Console.ReadKey();
+	Menu();
+}
+
+static void Potenciacao()
+{
+	Console.Clear();
+	
+	Console.Write("Digite o valor da base: " );
+	double v1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+	
+	Console.Write("Digite o valor do expoente : " );
+	double v2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+	
+	Console.WriteLine();
+	double resultado = Math.Pow(v1,v2);
+	
+	Console.WriteLine($"O resultado da potencia é: {resultado}");
+	Console.ReadKey();
+	Menu();
+}
+
+static void Raiz()
+{
+	Console.Write("Digite o valor: ");
+	double v1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+	
+	Console.WriteLine();
+	double resultado = Math.Sqrt(v1);
+	
+	Console.WriteLine($"O resultado da raiz é: {resultado}");
+	Console.ReadKey();
+	Menu();
+}
